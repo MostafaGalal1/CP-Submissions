68617b59c124ba1a7e50ecb4b617e4857c1fa5e6
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:17:56 +0300

    Add problem `Tram`

---
+++ Codeforces/746/213430253.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+#include <random>
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename  T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const ll N = 2e5 + 5, inf = LONG_MAX, mod = 1e9 + 7;
+void fast() { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+
+ll mul( ll a,  ll b){
+    return 1LL * a * b %mod;
+}
+
+ll add( ll a , ll b){
+    return (a + b) %mod;
+}
+
+ll fp( ll b , ll p){
+    if(!p)
+        return 1;
+    ll temp = fp(b,p/2);
+    temp = mul(temp,temp)%mod;
+    if(p&1)
+        temp = mul(temp,b);
+    return temp%mod;
+}
+
+ll fact(ll n){
+    ll res = 1;
+    for (int i = 2; i <= n; ++i)
+        res *= i;
+    return res;
+}
+
+ll inv(ll i){
+    return i <= 1 ? i : mod - (long long)(mod/i) * inv(mod % i) % mod;
+}
+
+ll mex(vector<ll> a){
+    set<ll> s;
+    ll n = (ll)a.size();
+
+    for (int i = 0; i < n; ++i)
+        s.insert(a[i]);
+
+    ll cu = 0;
+    for(auto ch : s){
+        if (ch != cu)
+            break;
+        cu++;
+    }
+
+    return cu;
+}
+
+vector<bool> sieveOfEratosthenes(ll n) {
+    vector<bool> isPrime(n + 1, true);
+    isPrime[0] = isPrime[1] = false;
+    for (int i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = 2 * i; j <= n; j += i)
+                isPrime[j] = false;
+        }
+    }
+    return isPrime;
+}
+
+ll divis(ll n) {
+    ll r = 1;
+    for (ll i = 1; i <= n/2; ++i) {
+        if (n%r)
+            return r;
+        r++;
+    }
+    return r;
+}
+
+int main() {
+    fast();
+    ll t;
+//    cin >> t;
+    t = 1;
+
+    while (t--) {
+        ll s, x, y, f, g, p, d;
+        cin >> s >> x >> y >> f >> g >> p >> d;
+
+        if (g <= f)
+            cout << abs(y - x) * g << endl;
+        else {
+            ll res = 0;
+            if (d > 0 && y-x > 0){
+                if (p > x)
+                    res += (s-p)*f + s*f + y*f;
+                else
+                    res += (y-p)*f;
+            } else if (d < 0 && y-x > 0){
+                res += p*f + y*f;
+            } else if (d < 0 && y-x < 0){
+                if (p < x)
+                    res += p*f + s*f + (s-y)*f;
+                else
+                    res += (-y+p) * f;
+            } else if (d > 0 && y-x < 0){
+                res += (s-p)*f + (s-y)*f;
+            }
+            cout << min(res, abs(y - x) * g) << endl;
+        }
+    }
+
+    return 0;
+}
