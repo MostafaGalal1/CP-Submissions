16b2c91259721b28cc6146b53ab809e6de4133ee
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:05:21 +0300

    Add problem `Mathematical Addition`

---
+++ Codeforces/1589/220500641.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <random>
+
+using namespace std;
+typedef long long ll;
+
+ll N = 2e5 + 5, inf = LONG_MAX, mod = 1e9 + 7;
+void fast() { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+
+int main() {
+    fast();
+    ll t;
+    cin >> t;
+//    t = 1;
+
+    while (t--) {
+        ll a, b;
+        cin >> a >> b;
+        cout << a*a << ' ' << -b*b << endl;
+    }
+
+    return 0;
+}
