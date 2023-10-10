2bd839d2eba7b12ab2e6845c891c1d07ac1c8971
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:27:24 +0300

    Add problem `Greatest Convex`

---
+++ Codeforces/1768/188051278.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef unsigned long long ll;
+const ll N = 1e5+7, mod = 1e9+7;
+const long double PI = 3.14159265358979323846264;
+
+void fast() { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+
+int main()
+{
+    fast();
+    ll t;
+    cin >> t;
+
+    while (t --> 0) {
+        ll n;
+        cin >> n;
+
+        cout << n-1 << endl;
+    }
+    return 0;
+}
