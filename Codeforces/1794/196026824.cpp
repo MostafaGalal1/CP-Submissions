5815a681b924b3780d86c0508cefa72078665804
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:24:36 +0300

    Add problem `Not Dividing`

---
+++ Codeforces/1794/196026824.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+const ll N = 105, mod = 1e9 + 7;
+const long double PI = 3.14159265358979323846264;
+
+void fast() { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+
+ll mul( ll a,  ll b){
+    return 1LL * a * b %mod;
+}
+
+ll add( ll a , ll b){
+    return (a + b) %mod;
+}
+
+ll fp( ll b , ll p){
+    if(!p)
+        return 1;
+    ll temp = fp(b,p/2);
+    temp = mul(temp,temp);
+    if(p&1)
+        temp = mul(temp,b);
+    return temp %mod;
+}
+
+ll fact(ll n){
+    ll res = 1;
+    for (int i = 1; i <= n; ++i)
+        res = mul(res, i);
+    return res;
+}
+
+ll inv(ll i){
+    return i <= 1 ? i : mod - (long long)(mod/i) * inv(mod % i) % mod;
+}
+
+ll func (ll n, ll k, vector<ll> & a){
+    ll sum = 0;
+    for (int j = 0; j < n; ++j) {
+        sum += min(a[j], k);
+    }
+    return sum;
+}
+
+int main() {
+    fast();
+    ll t;
+    cin >> t;
+
+    while (t --> 0) {
+        ll n;
+        bool done = false;
+        cin >> n;
+        vector<ll> a(n);
+
+        for (int i = 0; i < n; ++i) {
+            cin >> a[i];
+        }
+
+        for (int i = 0; i < n-1; ++i) {
+            if (a[i + 1] % a[i] == 0) {
+                a[i]++;
+            }
+        }
+
+        for (int i = 0; i < n-1; ++i) {
+            if (a[i + 1] % a[i] == 0) {
+                a[i + 1]++;
+            }
+        }
+
+        for (int i = 0; i < n; ++i) {
+            cout << a[i] << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
