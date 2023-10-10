8d663b4a9856398f8756d288d841ab436446b137
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:33:08 +0300

    Add problem `Maximum GCD`

---
+++ Codeforces/1370/169592482.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t --> 0){
+        int n;
+        cin >> n;
+
+        cout << n/2 << endl;
+    }
+}
