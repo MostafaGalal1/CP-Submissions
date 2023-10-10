090698f6ae3ae541f0b9b870187cca058349742b
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:28:33 +0300

    Add problem `Permutation Value`

---
+++ Codeforces/1743/176710611.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+typedef long long ll;
+using namespace std;
+
+int fact(int n);
+
+int nCr(int n, int r)
+{
+    return fact(n) / (fact(r) * fact(n - r));
+}
+
+// Returns factorial of n
+int fact(int n)
+{
+    if(n==0)
+        return 1;
+    int res = 1;
+    for (int i = 2; i <= n; i++)
+        res = res * i;
+    return res;
+}
+
+int main() {
+
+    ll t;
+    cin >> t;
+
+    while (t-- > 0) {
+        ll n;
+        cin >> n;
+        bool done = false;
+
+        cout << 1 << ' ';
+        for (ll i = n; i > 1; --i) {
+            cout << i << ' ';
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
