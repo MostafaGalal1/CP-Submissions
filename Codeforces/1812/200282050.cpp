e5c5a363aa19696f0ac1f48e6cfd3c3b3bc1deb8
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:23:21 +0300

    Add problem `Not a Geometry Problem`

---
+++ Codeforces/1812/200282050.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+const ll N = 105, mod = 1e9 + 7;
+const long double PI = 3.14159265358979323846264;
+const int siz = 3e5 + 10, inf = INT_MAX;
+
+int main() {
+
+    ll n, m, k;
+    cin >> n >> m >> k;
+    cout << 1.7320508075688772 << endl;
+    return 0;
+}
