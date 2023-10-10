2d111c71002ec4095e0a242abbb1d8677269c004
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:23:24 +0300

    Add problem `Trivial Conjecture`

---
+++ Codeforces/1812/200270620.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+const ll N = 105, mod = 1e9 + 7;
+const long double PI = 3.14159265358979323846264;
+const int siz = 3e5 + 10, inf = INT_MAX;
+
+int main() {
+
+    ll n;
+    cin >> n;
+    cout << 0 << endl;
+    return 0;
+}
