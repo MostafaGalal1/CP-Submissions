25a3fc8f1419de298720c7cf8b508a12daf0ef6d
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:39:03 +0300

    Add problem `Permutation Forgery`

---
+++ Codeforces/1405/161721780.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		for (int i = 0; i< t; i++){
+		    int n = sc.nextInt();
+		    int[] arr = new int[n];
+
+		    for (int j = 0 ; j < n; j++){
+		        arr[j] = sc.nextInt();
+		    }
+		    
+		    for (int j = 0; j < n; j++){
+		        System.out.print(arr[n-1-j] + " ");
+		    }
+		    System.out.println();
+		}
+	}
+}
