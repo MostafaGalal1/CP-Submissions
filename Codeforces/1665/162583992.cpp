c0f94cb3ee987083e0caed7cff8b39ff76177f3c
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:38:00 +0300

    Add problem `GCD vs LCM`

---
+++ Codeforces/1665/162583992.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    for(int i = 0; i < t; i++) {
+        int n = 0;
+        cin >> n;
+        cout << n - 3 << " " << 1 << " " << 1 << " " << 1 << endl;
+    }
+    return 0;
+}
