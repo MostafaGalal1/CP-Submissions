4a9396de8a63a78cd061fd062ae2e08d014916b8
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:23:54 +0300

    Add problem `Find and Replace`

---
+++ Codeforces/1807/198305565.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+const ll N = 105, mod = 1e9 + 7;
+const long double PI = 3.14159265358979323846264;
+const int siz = 1e5 + 10, inf = INT_MAX;
+
+vector<bool> vis(siz);
+vector<ld> dis(siz);
+
+void fast() { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+
+ll mul( ll a,  ll b){
+    return 1LL * a * b %mod;
+}
+
+ll add( ll a , ll b){
+    return (a + b) %mod;
+}
+
+ll fp( ll b , ll p){
+    if(!p)
+        return 1;
+    ll temp = fp(b,p/2);
+    temp = mul(temp,temp);
+    if(p&1)
+        temp = mul(temp,b);
+    return temp %mod;
+}
+
+ll fact(ll n){
+    ll res = 1;
+    for (int i = 1; i <= n; ++i)
+        res = mul(res, i);
+    return res;
+}
+
+ll inv(ll i){
+    return i <= 1 ? i : mod - (long long)(mod/i) * inv(mod % i) % mod;
+}
+
+ll func (ll n, ll k, vector<ll> & a){
+    ll sum = 0;
+    for (int j = 0; j < n; ++j) {
+        sum += min(a[j], k);
+    }
+    return sum;
+}
+
+void bfs(ll node, vector<vector<ll>> &arr){
+    queue<ll> q;
+    q.push(node);
+    dis[node] = 0;
+
+    while (!q.empty()) {
+        node = q.front();
+        q.pop();
+
+        for(auto child : arr[node]){
+            if (dis[node] + 1 < dis[child]) {
+                dis[child] = dis[node] + 1;
+                q.push(child);
+            }
+        }
+    }
+}
+
+ll max_cats;
+ll dfs(ll node, ll cats_no, vector<ll> &cats, vector<vector<ll>> &arr) {
+    vis[node] = true;
+    ll pt = 0;
+
+    if (cats_no > max_cats)
+        return 0;
+
+    if (arr[node].size() == 1 && node)
+        return 1;
+
+    for (auto child : arr[node]) {
+        if (!vis[child])
+            pt += dfs(child, cats[child] + (cats[node] > 0? cats_no:0), cats, arr);
+    }
+
+    return pt;
+}
+
+int main() {
+    ll t;
+    cin >> t;
+
+    while (t --> 0) {
+        string s;
+        ll n, nu;
+        bool done = false;
+        cin >> n >> s;
+        vector<vector<ll>> a(26, vector<ll>());
+
+        for (int i = 0; i < n; ++i) {
+            a[s[i]-97].push_back(i);
+        }
+
+        for (int i = 0; i < 26 && !done; ++i) {
+            ll o;
+            if (!a[i].empty())
+                o = a[i][0]%2;
+            for (int j = 1; j < a[i].size(); ++j) {
+                if (o != a[i][j]%2){
+                    done = true;
+                    break;
+                }
+            }
+        }
+        
+        if (!done)
+            cout << "YES";
+        else
+            cout << "NO";
+        cout << endl;
+    }
+    return 0;
+}
