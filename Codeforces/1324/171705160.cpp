b40b4727d67a7ce5ff0600bc54e27015ab456535
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:31:08 +0300

    Add problem `Yet Another Tetris Problem`

---
+++ Codeforces/1324/171705160.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+void fast() { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+
+int main()
+{
+    fast();
+
+    ll t;
+    cin >> t;
+
+    while (t --> 0) {
+        ll n;
+        bool done = false;
+        cin >> n;
+        vector<ll> a(n);
+
+        for (int i = 0; i < n; ++i) {
+            cin >> a[i];
+        }
+
+        for (int i = 1; i < n; ++i) {
+            if (abs(a[i] - a[i-1])%2) {
+                cout << "NO";
+                done = true;
+                break;
+            }
+        }
+
+        if (!done)
+            cout << "YES";
+        cout << endl;
+
+    }
+
+    return 0;
+}
