ce0343b844bd10e33a1f7daf4c7d83c4dfb3ecff
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:39:49 +0300

    Add problem `Again Twenty Five!`

---
+++ Codeforces/630/159614035.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong();
+		System.out.println(25);
+	}
+}
