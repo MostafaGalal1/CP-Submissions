c79e2fb9413237d02a8c5d0a0f68c10fc5cf03a6
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:05:13 +0300

    Add problem `Divisor Chain`

---
+++ Codeforces/1864/220559877.cpp
@@ -0,0 +1,163 @@
+#include <bits/stdc++.h>
+#include <random>
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename  T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+ll N = 2e5 + 5, inf = LONG_MAX, mod = 1e9 + 7;
+void fast() { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+
+ll mul( ll a,  ll b){
+    return 1LL * a * b %mod;
+}
+
+ll add( ll a , ll b){
+    return (a + b) %mod;
+}
+
+ll fp( ll b , ll p){
+    if(!p)
+        return 1;
+    ll temp = fp(b,p/2);
+    temp = mul(temp,temp)%mod;
+    if(p&1)
+        temp = mul(temp,b);
+    return temp%mod;
+}
+
+ll fact(ll n){
+    ll res = 1;
+    for (int i = 2; i <= n; ++i)
+        res *= i;
+    return res;
+}
+
+ll inv(ll i){
+    return i <= 1 ? i : mod - (long long)(mod/i) * inv(mod % i) % mod;
+}
+
+ll mex(vector<ll> a){
+    set<ll> s;
+    ll n = (ll)a.size();
+
+    for (int i = 0; i < n; ++i)
+        s.insert(a[i]);
+
+    ll cu = 0;
+    for(auto ch : s){
+        if (ch != cu)
+            break;
+        cu++;
+    }
+
+    return cu;
+}
+
+vector<ll> sieveOfEratosthenes(ll n) {
+    vector<bool> isPrime(n + 1, true);
+    vector<ll> v;
+    isPrime[0] = isPrime[1] = false;
+    for (int i = 2; i <= n; i++) {
+        if (isPrime[i]) {
+            if (i * i <= n) {
+                v.push_back(i);
+                for (int j = 2 * i; j <= n; j += i)
+                    isPrime[j] = false;
+            } else
+                v.push_back(i);
+        }
+    }
+    return v;
+}
+
+vector<ll> divis(ll n) {
+    vector<ll> r;
+    for (ll i = 1; i <= (ll)sqrt(n); ++i) {
+        if (n % i == 0) {
+            r.push_back(i);
+            if (i != n / i)
+                r.push_back(n / i);
+        }
+    }
+    return r;
+}
+
+void primeFactors(ll n, map<ll, ll>& m) {
+    while (n % 2 == 0) {
+        m[2]++;
+        n /= 2;
+    }
+    for (ll i = 3; i <= sqrt(n); i += 2) {
+        while (n % i == 0) {
+            m[i]++;
+            n /= i;
+        }
+    }
+    if (n > 2)
+        m[n]++;
+}
+
+ll fibonacci(ll n) {
+    double phi = (1 + sqrt(5)) / 2;
+    double psi = (1 - sqrt(5)) / 2;
+    double fn = (pow(phi, n) - pow(psi, n)) / sqrt(5);
+    return static_cast<ll>(round(fn));
+}
+
+ll dfs(ll node, vector<vector<ll>>& adj, vector<bool>& vis, vector<ll>& val, vector<ll>& res) {
+    vis[node] = true;
+    ll xo = val[node];
+    for (auto ch : adj[node]) {
+        if (!vis[ch])
+            xo ^= dfs(ch, adj, vis, val, res);
+    }
+    return res[node] = xo;
+}
+
+int main() {
+    fast();
+    ll t;
+    cin >> t;
+//    t = 1;
+
+    while (t --> 0) {
+        vector<ll> res, ress;
+        ll n;
+        cin >> n;
+
+        ll cur = 1;
+        while (cur <= n){
+            res.push_back(cur);
+            cur *= 2;
+        }
+
+        cur = n-res[res.size()-1];
+        for (int i = 0; i < 32; ++i) {
+            if (cur & 1)
+                ress.push_back(1ll << i);
+            cur >>= 1;
+        }
+
+        sort(res.rbegin(), res.rend());
+
+        cout << (ll)res.size() + (ll)ress.size() << endl;
+        cur = n;
+        for (int i = 0; i < (ll)ress.size(); ++i) {
+            cout << cur << " ";
+            cur -= ress[i];
+        }
+        for (int i = 1; i < (ll)res.size(); ++i) {
+            cout << cur << " ";
+            cur -= res[i];
+        }
+        cout << 1 << endl;
+    }
+    return 0;
+}
