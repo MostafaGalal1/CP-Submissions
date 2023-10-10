00f9f255a441d18c91afe74458a0ce667c77f2de
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:33:07 +0300

    Add problem `EhAb AnD gCd`

---
+++ Codeforces/1325/169595614.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t --> 0){
+        ll n;
+        cin >> n;
+        cout << 1 << ' ' << (n-1) << endl;
+    }
+}
