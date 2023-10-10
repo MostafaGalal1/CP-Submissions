69fc1e49b5ab61fa44d01a6285788bdebf543129
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:33:00 +0300

    Add problem `Digits Sum`

---
+++ Codeforces/1553/169597853.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t --> 0){
+        ll n;
+        cin >> n;
+        
+        cout << (n+1)/10 << endl;
+    }
+}
