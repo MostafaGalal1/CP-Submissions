f71e36f6d6596359d1c8a82b032fe3ce90c2f607
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:24:20 +0300

    Add problem `Jzzhu and Sequences`

---
+++ Codeforces/450/197303050.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+const ll N = 105, mod = 1e9 + 7;
+const long double PI = 3.14159265358979323846264;
+
+void fast() { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+
+ll mul( ll a,  ll b){
+    return 1LL * a * b %mod;
+}
+
+ll add( ll a , ll b){
+    return (a + b) %mod;
+}
+
+ll fp( ll b , ll p){
+    if(!p)
+        return 1;
+    ll temp = fp(b,p/2);
+    temp = mul(temp,temp);
+    if(p&1)
+        temp = mul(temp,b);
+    return temp %mod;
+}
+
+ll fact(ll n){
+    ll res = 1;
+    for (int i = 1; i <= n; ++i)
+        res = mul(res, i);
+    return res;
+}
+
+ll inv(ll i){
+    return i <= 1 ? i : mod - (long long)(mod/i) * inv(mod % i) % mod;
+}
+
+ll func (ll n, ll k, vector<ll> & a){
+    ll sum = 0;
+    for (int j = 0; j < n; ++j) {
+        sum += min(a[j], k);
+    }
+    return sum;
+}
+
+int main() {
+    fast();
+
+        ll n, x, y;
+        cin >> x >> y;
+        cin >> n;
+        n--;
+
+        ll a[6] = {x, y, add(y, -x), -x, -y,add(x, -y)};
+        cout << (a[n%6]+mod)%mod << endl;
+    return 0;
+}
