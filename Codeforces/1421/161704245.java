b1edc37fc20348281eba84a659d0f455b42cf867
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:39:14 +0300

    Add problem `XORwice`

---
+++ Codeforces/1421/161704245.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		for (int i = 0; i < t; i++){
+		    int a = sc.nextInt();
+		    int b = sc.nextInt();
+		    int c = a & b;
+		    System.out.println((a ^ c) + (b ^ c));
+		}
+	}
+}
