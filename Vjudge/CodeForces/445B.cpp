95099b46c49559fae2595c02ea498bc928365bfc
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:13:32 +0300

    Add problem `DZY Loves Chemistry`

---
+++ Vjudge/CodeForces/445B.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+const ll N = 3e4 + 10;
+ll num;
+vector<ll> arr[N];
+bool visited[N];
+
+void dfs (ll node) {
+    visited[node] = true;
+    for (auto child : arr[node]){
+        if (!visited[child]) {
+            num++;
+            dfs(child);
+        }
+    }
+}
+
+int main() {
+
+    ll n, m;
+    cin >> n >> m;
+
+    for (ll i = 1; i <= m; i++) {
+        ll u, v;
+        cin >> u >> v;
+        arr[u].push_back(v);
+        arr[v].push_back(u);
+    }
+
+    for (ll i = 1; i <= n; i++) {
+        dfs(i);
+    }
+
+    cout << (ll)pow(2, num) << endl;
+
+    return 0;
+}
