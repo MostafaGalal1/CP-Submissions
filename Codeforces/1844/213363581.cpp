2b8e00930903f6d6159db7110ba6a4177ec10932
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:10:05 +0300

    Add problem `Particles`

---
+++ Codeforces/1844/213363581.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+#include <random>
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename  T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const ll N = 2e5 + 5, inf = LONG_MAX, mod = 1e9 + 7;
+void fast() { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+
+ll mul( ll a,  ll b){
+    return 1LL * a * b %mod;
+}
+
+ll add( ll a , ll b){
+    return (a + b) %mod;
+}
+
+ll fp( ll b , ll p){
+    if(!p)
+        return 1;
+    ll temp = fp(b,p/2);
+    temp = mul(temp,temp)%mod;
+    if(p&1)
+        temp = mul(temp,b);
+    return temp%mod;
+}
+
+ll fact(ll n){
+    ll res = 1;
+    for (int i = 2; i <= n; ++i)
+        res *= i;
+    return res;
+}
+
+ll inv(ll i){
+    return i <= 1 ? i : mod - (long long)(mod/i) * inv(mod % i) % mod;
+}
+
+ll mex(vector<ll> a){
+    set<ll> s;
+    ll n = (ll)a.size();
+
+    for (int i = 0; i < n; ++i)
+        s.insert(a[i]);
+
+    ll cu = 0;
+    for(auto ch : s){
+        if (ch != cu)
+            break;
+        cu++;
+    }
+
+    return cu;
+}
+
+vector<bool> sieveOfEratosthenes(ll n) {
+    vector<bool> isPrime(n + 1, true);
+    isPrime[0] = isPrime[1] = false;
+    for (int i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = 2 * i; j <= n; j += i)
+                isPrime[j] = false;
+        }
+    }
+    return isPrime;
+}
+
+int main() {
+    fast();
+    ll t;
+    cin >> t;
+//    t = 1;
+
+    while (t--) {
+        ll n;
+        bool b = false;
+        cin >> n;
+        vector<ll> a(n);
+
+        ll mn = LONG_LONG_MIN;
+        for (int i = 0; i < n; ++i) {
+            cin >> a[i];
+            if (a[i] < 0)
+                mn = max(mn, a[i]);
+            else
+                b = true;
+        }
+
+        if (b) {
+            ll r = 0, s = 0;
+            for (int i = 0; i < n; ++i) {
+                if (i % 2)
+                    s += max(a[i], 0ll);
+                else
+                    r += max(a[i], 0ll);
+            }
+            cout << max(r, s) << endl;
+        } else
+            cout << mn << endl;
+    }
+
+    return 0;
+}
