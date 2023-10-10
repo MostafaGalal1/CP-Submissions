f0335f9c8a87645999f1136ff9ce2f094cf07daa
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:01:01 +0300

    Add problem `Jellyfish and Green Apple`

---
+++ Codeforces/1875/225991265.cpp
@@ -0,0 +1,170 @@
+#include <bits/stdc++.h>
+#include <random>
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+const ll N = 2e6+5, inf = LONG_MAX, mod = 998244353;
+void fast() { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+ll add ( ll a , ll b)
+{
+    return (a + b) % mod;
+}
+ll mul ( ll a , ll b)
+{
+    return 1LL * a * b % mod;
+}
+
+ll fp( ll b , ll p)
+{
+    if(!p)
+        return 1;
+    ll temp = fp(b,p/2);
+    temp = mul(temp,temp);
+    if(p&1)
+        temp = mul(temp,b);
+    return temp;
+}
+
+ll fact[N], inv[N];
+void build()
+{
+    fact[0] = fact[1] = inv[0] = inv[1] = 1;
+    for (ll i = 2; i < N; i++)
+    {
+        fact[i] = fact[i - 1] * i % mod;
+        inv[i] = inv[mod % i] * (mod - mod / i) % mod;
+    }
+    for (ll i = 2; i < N; i++)
+        inv[i] = inv[i] * inv[i - 1] % mod;
+}
+ll nCr( ll n , ll r)
+{
+    return mul(fact[n],mul(inv[n-r],inv[r]));
+}
+ll nPr(ll n, ll r)
+{
+    ll ans = nCr(n, r); ans *= fact[r];
+    ans %= mod;
+    return ans;
+}
+
+ll mex(vector<ll> a){
+    set<ll> s;
+    ll n = (ll)a.size();
+
+    for (int i = 0; i < n; ++i)
+        s.insert(a[i]);
+
+    ll cu = 0;
+    for(auto ch : s){
+        if (ch != cu)
+            break;
+        cu++;
+    }
+
+    return cu;
+}
+
+vector<bool> isPrime(2000001, true);
+void sieveOfEratosthenes() {
+    ll n = 2000000;
+    isPrime[0] = isPrime[1] = false;
+    for (int i = 2; i <= n; i++) {
+        if (isPrime[i]) {
+            for (int j = 2 * i; j <= n; j += i)
+                isPrime[j] = false;
+        }
+    }
+}
+
+double fibonacci(ll n) {
+    double phi = (1 + sqrt(5)) / 2;
+    double psi = (1 - sqrt(5)) / 2;
+    double fn = (pow(phi, n) - pow(psi, n)) / sqrt(5);
+    return round(fn);
+}
+
+vector<ll> divis(ll n) {
+    vector<ll> r;
+    for (ll i = 1; i <= (ll)sqrt(n); ++i) {
+        if (n % i == 0) {
+            r.push_back(i);
+            if (n / i != i)
+                r.push_back(n / i);
+        }
+    }
+    return r;
+}
+
+void primeFactors(ll n, map<ll, ll>& m) {
+    while (n % 2 == 0) {
+        m[2]++;
+        n /= 2;
+    }
+    for (ll i = 3; i <= sqrt(n); i += 2) {
+        while (n % i == 0) {
+            m[i]++;
+            n /= i;
+        }
+    }
+    if (n > 2)
+        m[n]++;
+}
+
+vector<ll> sieveOfEratosthenes(ll n) {
+    vector<bool> isPrime(n + 1, true);
+    vector<ll> v;
+    isPrime[0] = isPrime[1] = false;
+    for (int i = 2; i <= n; i++) {
+        if (isPrime[i]) {
+            if (i * i <= n) {
+                v.push_back(i);
+                for (int j = 2 * i; j <= n; j += i)
+                    isPrime[j] = false;
+            } else
+                v.push_back(i);
+        }
+    }
+    return v;
+}
+
+ll dx[] = {-1, 1, 0, 0};
+ll dy[] = {0, 0, -1, 1};
+
+int main() {
+    fast();
+    ll t;
+    cin >> t;
+//    t = 1;
+
+    while (t --> 0) {
+        ll n, k;
+        cin >> n >> k;
+
+        if (k%2 && n%k)
+            cout << -1;
+        else if (n%k == 0)
+            cout << 0;
+        else if (k%2 == 0) {
+            n -= n/k*k;
+            ll cnt = 0, c = 0;
+            while (n && c < 50){
+                cnt += n;
+                c++;
+                if (2*n >= k)
+                    n = 2*n - k;
+                else
+                    n *= 2;
+            }
+            if (c == 50)
+                cout << -1;
+            else
+                cout << cnt;
+        }
+        cout << endl;
+
+    }
+    return 0;
+}
