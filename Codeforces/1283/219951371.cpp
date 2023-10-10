cbb300eaaf4b184855b4471efa557153c2b181a5
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:05:34 +0300

    Add problem `Candies Division`

---
+++ Codeforces/1283/219951371.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <random>
+
+using namespace std;
+typedef long long ll;
+
+ll N = 2e5 + 5, inf = LONG_MAX, mod = 1e9 + 7;
+void fast() { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+
+int main() {
+    fast();
+    ll t;
+    cin >> t;
+//    t = 1;
+
+    while (t --> 0) {
+        ll h, m;
+        cin >> h >> m;
+
+        cout << h/m*m + min(h%m, m/2) << endl;
+    }
+    return 0;
+}
