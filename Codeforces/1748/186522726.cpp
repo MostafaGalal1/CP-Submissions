17dced11acef8557c8909c0faa03e71052ad69c8
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:27:32 +0300

    Add problem `The Ultimate Square`

---
+++ Codeforces/1748/186522726.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+const ll N = 1e5+7, mod = 1e9+7;
+const long double PI = 3.14159265358979323846264;
+
+void fast() { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+
+int main()
+{
+    fast();
+    ll t;
+    cin >> t;
+
+    while (t --> 0) {
+        bool done = false;
+        ll n;
+        cin >> n;
+
+        cout << (n%2==0?n/2:n/2+1) << endl;
+    }
+    return 0;
+}
