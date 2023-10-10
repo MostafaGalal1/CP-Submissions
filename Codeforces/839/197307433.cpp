1d27b04d7823de8e15caa8dceafeec210ec185ec
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:24:16 +0300

    Add problem `Journey`

---
+++ Codeforces/839/197307433.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+const ll N = 105, mod = 1e9 + 7;
+const long double PI = 3.14159265358979323846264;
+const int siz = 1e5 + 10, inf = INT_MAX;
+
+vector<bool> vis(siz);
+vector<ld> dis(siz);
+
+void fast() { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+
+ll mul( ll a,  ll b){
+    return 1LL * a * b %mod;
+}
+
+ll add( ll a , ll b){
+    return (a + b) %mod;
+}
+
+ll fp( ll b , ll p){
+    if(!p)
+        return 1;
+    ll temp = fp(b,p/2);
+    temp = mul(temp,temp);
+    if(p&1)
+        temp = mul(temp,b);
+    return temp %mod;
+}
+
+ll fact(ll n){
+    ll res = 1;
+    for (int i = 1; i <= n; ++i)
+        res = mul(res, i);
+    return res;
+}
+
+ll inv(ll i){
+    return i <= 1 ? i : mod - (long long)(mod/i) * inv(mod % i) % mod;
+}
+
+ll func (ll n, ll k, vector<ll> & a){
+    ll sum = 0;
+    for (int j = 0; j < n; ++j) {
+        sum += min(a[j], k);
+    }
+    return sum;
+}
+
+void bfs(ll node, vector<vector<ll>> &arr){
+    queue<ll> q;
+    q.push(node);
+    dis[node] = 0;
+
+    while (!q.empty()) {
+        node = q.front();
+        q.pop();
+
+        for(auto child : arr[node]){
+            if (dis[node] + 1 < dis[child]) {
+                dis[child] = dis[node] + 1;
+                q.push(child);
+            }
+        }
+    }
+}
+
+void dfs(ll node, vector<vector<ll>> &arr){
+    vis[node] = true;
+    for (auto child : arr[node]){
+        if (!vis[child]) {
+            dfs(child, arr);
+            if (node)
+                dis[node] += dis[child] / (arr[node].size() - 1);
+            else
+                dis[node] += dis[child] / arr[node].size();
+        }
+    }
+
+    if (node)
+        dis[node] += 1;
+}
+
+int main() {
+    fast();
+
+    ll n, x, y;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        vis[i] = false;
+        dis[i] = 0;
+    }
+
+    vector<vector<ll>> arr(n, vector<ll>());
+
+    for (int i = 0; i < n-1; ++i) {
+        cin >> x >> y;
+        x--, y--;
+        arr[x].push_back(y);
+        arr[y].push_back(x);
+    }
+
+    dfs(0, arr);
+    cout << setprecision(12) << dis[0] << endl;
+    return 0;
+}
