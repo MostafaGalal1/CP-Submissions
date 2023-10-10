35641d4656fd725950cf97d2dcc65d9c576865df
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:30:12 +0300

    Add problem `Specific Tastes of Andre `

---
+++ Codeforces/1438/174676520.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+    ll t;
+    cin >> t;
+
+    while (t --> 0){
+        ll n;
+        cin >> n;
+
+        for (int i = 0; i < n; ++i) {
+            cout << n << ' ';
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
