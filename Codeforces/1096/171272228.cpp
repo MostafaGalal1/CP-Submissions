605e362000c300acf8de83b0d6e34e932a079308
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:31:59 +0300

    Add problem `Find Divisible`

---
+++ Codeforces/1096/171272228.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main(){
+    ll t;
+    cin >> t;
+
+    while (t --> 0) {
+        ll l, r;
+        cin >> l >> r;
+
+        cout << l << ' ' << l * 2 << endl;
+    }
+    return 0;
+}
