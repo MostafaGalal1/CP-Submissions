8cf860951a9009330941aa292941267624938e3a
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:25:18 +0300

    Add problem `Broken checker`

---
+++ Codeforces/171/193606522.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+const ll N = 1e5+7, mod = 1e9+7;
+const long double PI = 3.14159265358979323846264;
+
+void fast() { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+
+int main()
+{
+    fast();
+    ll t;
+    cin >> t;
+
+    if (t == 5)
+        cout << 1 << endl;
+    else
+        cout << t%3 + 1 << endl;
+
+    return 0;
+}
