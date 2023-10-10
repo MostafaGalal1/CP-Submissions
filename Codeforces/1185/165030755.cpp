9f78df7f44b654ac3f7ae45d88401b8301cbf8d0
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:36:31 +0300

    Add problem `Email from Polycarp`

---
+++ Codeforces/1185/165030755.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <cmath>
+#include <algorithm>
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t --> 0){
+        bool done = false;
+        string s1, s2;
+        int len1, len2, i = 0, j = 0;
+        cin >> s1 >> s2;
+        len1 = (int)s1.length();
+        len2 = (int)s2.length();
+        unordered_map<char, int> m;
+
+        while (i < len1 || j < len2){
+            if (s1[i] != s2[j]){
+                if (s2[j] == s2[j-1] && j > 0)
+                    j++;
+                else{
+                    done = true;
+                    break;
+                }
+            } else {
+                i++;
+                j++;
+            }
+        }
+
+        if (!done)
+            cout << "YES";
+        else
+            cout << "NO";
+
+        cout << endl;
+    }
+}
