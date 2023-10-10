5e8fe77e32b51aa2a50cfb69dd27b5c303d589fa
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:06:13 +0300

    Add problem Towers

---
+++ CSES/Sorting_and_Searching/Towers.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <random>
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+const ll N = 2e5 + 5, inf = LONG_MAX, mod = 1e9 + 7;
+void fast() { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+
+int main() {
+    fast();
+    ll t;
+//    cin >> t;
+    t = 1;
+
+    while (t--) {
+        ll n, nu;
+        cin >> n;
+        vector<ll> a;
+
+
+        ll cnt = 0;
+        for (int i = 0; i < n; ++i) {
+            cin >> nu;
+            auto it = lower_bound(a.begin(), a.end(), nu+1);
+            if (it != a.end()){
+                *it = nu;
+            } else {
+                a.push_back(nu);
+                cnt++;
+            }
+        }
+
+        cout << cnt << endl;
+    }
+
+    return 0;
+}
