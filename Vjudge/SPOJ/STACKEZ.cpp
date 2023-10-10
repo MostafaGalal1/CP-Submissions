59587400ea43c7a0a184b0927e29e28a46f895d0
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:13:12 +0300

    Add problem `Easy Stack`

---
+++ Vjudge/SPOJ/STACKEZ.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+    int t;
+    cin >> t;
+    stack<int> s;
+
+    while(t --> 0){
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+
+        int op, n;
+        cin >> op;
+
+        if (op == 1) {
+            cin >> n;
+            s.push(n);
+        } else if (op == 2) {
+            if (!s.empty())
+                s.pop();
+        } else if (op == 3) {
+            if (s.empty())
+                cout << "Empty!\n";
+            else
+                cout << s.top() << "\n";
+        }
+    }
+    return 0;
+}
