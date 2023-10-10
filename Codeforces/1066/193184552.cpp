81441c2242ba04fc74b19f1e9fdb99e06a1070fd
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:25:50 +0300

    Add problem `Vova and Train`

---
+++ Codeforces/1066/193184552.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+const ll N = 1e5+7, mod = 1e9+7;
+const long double PI = 3.14159265358979323846264;
+
+void fast() { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+
+int main()
+{
+    fast();
+
+    ll t;
+    cin >> t;
+
+    while (t --> 0){
+        ll lo, v, l, r;
+        cin >> lo >> v >> l >> r;
+        cout << lo/v - r/v + (l-1)/v << endl;
+    }
+}
