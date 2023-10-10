30c55ad77ea1d55fedd5fb67a513c1c4c50e8928
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:32:59 +0300

    Add problem `Strange Functions`

---
+++ Codeforces/1455/169598459.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t --> 0){
+        string n;
+        cin >> n;
+
+        cout << n.length() << endl;
+    }
+}
