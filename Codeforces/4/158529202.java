1dcf512279276f44b4847b76d27918a4b5c4f482
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:40:54 +0300

    Add problem `Watermelon`

---
+++ Codeforces/4/158529202.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int w = sc.nextInt();
+
+		if (w%2 == 0 && w != 2)
+		    System.out.println("YES");
+		else
+		    System.out.println("NO");
+	}
+}
