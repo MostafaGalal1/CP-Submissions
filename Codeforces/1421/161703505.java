0d5c52d5a4f200128b914de488ab28c5cff17e08
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:39:15 +0300

    Add problem `Palindromifier`

---
+++ Codeforces/1421/161703505.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		int n = s.length();
+		System.out.println(3 + "\nL " + 2 + "\nR " + 2 + "\nR " + (2*n-1));
+	}
+}
