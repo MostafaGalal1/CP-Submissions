4bad6c2b8acfda562527379f5ef7b561eb3f370b
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:24:22 +0300

    Add problem `GCD Problem`

---
+++ Codeforces/1617/197300591.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+const ll N = 105, mod = 1e9 + 7;
+const long double PI = 3.14159265358979323846264;
+
+void fast() { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+
+ll mul( ll a,  ll b){
+    return 1LL * a * b %mod;
+}
+
+ll add( ll a , ll b){
+    return (a + b) %mod;
+}
+
+ll fp( ll b , ll p){
+    if(!p)
+        return 1;
+    ll temp = fp(b,p/2);
+    temp = mul(temp,temp);
+    if(p&1)
+        temp = mul(temp,b);
+    return temp %mod;
+}
+
+ll fact(ll n){
+    ll res = 1;
+    for (int i = 1; i <= n; ++i)
+        res = mul(res, i);
+    return res;
+}
+
+ll inv(ll i){
+    return i <= 1 ? i : mod - (long long)(mod/i) * inv(mod % i) % mod;
+}
+
+ll func (ll n, ll k, vector<ll> & a){
+    ll sum = 0;
+    for (int j = 0; j < n; ++j) {
+        sum += min(a[j], k);
+    }
+    return sum;
+}
+
+int main() {
+    fast();
+    ll t;
+    cin >> t;
+
+    while(t --> 0) {
+        ll n;
+        cin >> n;
+
+        for (int i = 2; i < min((ll)sqrt(n)+10, n/2); ++i) {
+            if (gcd(i, n-1-i) == 1){
+                cout << i << ' ' << n-1-i << ' ' << 1 << endl;
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
