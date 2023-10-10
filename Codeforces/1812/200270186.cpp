a63a820ae61b0518b6a6d9f1c9cb7bdf39264c51
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:23:26 +0300

    Add problem `Was it Rated?`

---
+++ Codeforces/1812/200270186.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+const ll N = 105, mod = 1e9 + 7;
+const long double PI = 3.14159265358979323846264;
+const int siz = 3e5 + 10, inf = INT_MAX;
+
+int main() {
+
+    ll n;
+    cin >> n;
+    if (n != 15 && n != 20 && n != 21)
+        cout << "YES";
+    else
+        cout << "NO";
+    cout << endl;
+    return 0;
+}
