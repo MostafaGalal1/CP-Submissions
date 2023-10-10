ea860aba6d728750ee945b1ffbfaceb0c7828704
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:08:28 +0300

    Add problem `Prime Matrix`

---
+++ Vjudge/CodeForces/271B.cpp
@@ -0,0 +1,146 @@
+#include <bits/stdc++.h>
+#include <random>
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename  T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+ll N = 2e5 + 5, inf = LONG_MAX, mod = 1e9 + 7;
+void fast() { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+
+ll mul( ll a,  ll b){
+    return 1LL * a * b %mod;
+}
+
+ll add( ll a , ll b){
+    return (a + b) %mod;
+}
+
+ll fp( ll b , ll p){
+    if(!p)
+        return 1;
+    ll temp = fp(b,p/2);
+    temp = mul(temp,temp)%mod;
+    if(p&1)
+        temp = mul(temp,b);
+    return temp%mod;
+}
+
+ll fact(ll n){
+    ll res = 1;
+    for (int i = 2; i <= n; ++i)
+        res *= i;
+    return res;
+}
+
+ll inv(ll i){
+    return i <= 1 ? i : mod - (long long)(mod/i) * inv(mod % i) % mod;
+}
+
+ll mex(vector<ll> a){
+    set<ll> s;
+    ll n = (ll)a.size();
+
+    for (int i = 0; i < n; ++i)
+        s.insert(a[i]);
+
+    ll cu = 0;
+    for(auto ch : s){
+        if (ch != cu)
+            break;
+        cu++;
+    }
+
+    return cu;
+}
+
+vector<ll> sieveOfEratosthenes(ll n) {
+    vector<bool> isPrime(n + 1, true);
+    vector<ll> v;
+    isPrime[0] = isPrime[1] = false;
+    for (int i = 2; i <= n; i++) {
+        if (isPrime[i]) {
+            if (i * i <= n) {
+                v.push_back(i);
+                for (int j = 2 * i; j <= n; j += i)
+                    isPrime[j] = false;
+            } else
+                v.push_back(i);
+        }
+    }
+    return v;
+}
+
+vector<ll> divis(ll n) {
+    vector<ll> r;
+    for (ll i = 1; i <= (ll)sqrt(n); ++i) {
+        if (n % i == 0) {
+            r.push_back(i);
+            if (i != n / i)
+                r.push_back(n / i);
+        }
+    }
+    return r;
+}
+
+void primeFactors(ll n, map<ll, ll>& m) {
+    while (n % 2 == 0) {
+        m[2]++;
+        n /= 2;
+    }
+    for (ll i = 3; i <= sqrt(n); i += 2) {
+        while (n % i == 0) {
+            m[i]++;
+            n /= i;
+        }
+    }
+    if (n > 2)
+        m[n]++;
+}
+
+int main() {
+    fast();
+    ll t;
+//    cin >> t;
+    t = 1;
+
+    while (t --> 0) {
+        vector<ll> v = sieveOfEratosthenes(1e6);
+        ll n, m, nu;
+        cin >> n >> m;
+        vector<vector<ll>> a(n, vector<ll>(m));
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                cin >> nu;
+                a[i][j] = *lower_bound(v.begin(), v.end(), nu) - nu;
+            }
+        }
+
+        ll res = LONG_LONG_MAX, cnt;
+        for (int i = 0; i < n; ++i) {
+            cnt = 0;
+            for (int j = 0; j < m; ++j) {
+                cnt += a[i][j];
+            }
+            res = min(res, cnt);
+        }
+
+        for (int i = 0; i < m; ++i) {
+            cnt = 0;
+            for (int j = 0; j < n; ++j) {
+                cnt += a[j][i];
+            }
+            res = min(res, cnt);
+        }
+
+        cout << res << endl;;
+    }
+    return 0;
+}
