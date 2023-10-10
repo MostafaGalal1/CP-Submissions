15326cf9fe05c63b3322c548836b31f95beee3ba
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:06:46 +0300

    Add problem `k-Multiple Free Set`

---
+++ Codeforces/275/218389552.cpp
@@ -0,0 +1,180 @@
+#include <bits/stdc++.h>
+#include <random>
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename  T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const ll N = 2e6 + 5, inf = LONG_MAX, mod = 1e9 + 7;
+void fast() { ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr); }
+
+ll add ( ll a , ll b)
+{
+    return (a + b) % mod;
+}
+ll mul ( ll a , ll b)
+{
+    return 1LL * a * b % mod;
+}
+ll fp( ll b , ll p)
+{
+    if(!p)
+        return 1;
+    ll temp = fp(b,p/2);
+    temp = mul(temp,temp);
+    if(p&1)
+        temp = mul(temp,b);
+    return temp;
+}
+
+ll fact[N], inv[N];
+void build()
+{
+    fact[0] = fact[1] = inv[0] = inv[1] = 1;
+    for (ll i = 2; i < N; i++)
+    {
+        fact[i] = fact[i - 1] * i % mod;
+        inv[i] = inv[mod % i] * (mod - mod / i) % mod;
+    }
+    for (ll i = 2; i < N; i++)
+        inv[i] = inv[i - 1] * inv[i] % mod;
+}
+ll nCr( ll n , ll r)
+{
+    return mul(fact[n],mul(inv[n-r],inv[r]));
+}
+ll nPr(ll n, ll r)
+{
+    ll ans = nCr(n, r); ans *= fact[r];
+    ans %= mod;
+    return ans;
+}
+
+ll mex(vector<ll> a){
+    set<ll> s;
+    ll n = (ll)a.size();
+
+    for (int i = 0; i < n; ++i)
+        s.insert(a[i]);
+
+    ll cu = 0;
+    for(auto ch : s){
+        if (ch != cu)
+            break;
+        cu++;
+    }
+
+    return cu;
+}
+
+vector<ll> sieveOfEratosthenes(ll n) {
+    vector<bool> isPrime(n + 1, true);
+    vector<ll> v;
+    isPrime[0] = isPrime[1] = false;
+    for (int i = 2; i <= n; i++) {
+        if (isPrime[i]) {
+            if (i * i <= n) {
+                v.push_back(i);
+                for (int j = 2 * i; j <= n; j += i)
+                    isPrime[j] = false;
+            } else
+                v.push_back(i);
+        }
+    }
+    return v;
+}
+
+vector<ll> divis(ll n) {
+    vector<ll> r;
+    for (ll i = 1; i <= (ll)sqrt(n); ++i) {
+        if (n % i == 0) {
+            r.push_back(i);
+            if (i != n / i)
+                r.push_back(n / i);
+        }
+    }
+    return r;
+}
+
+void primeFactors(ll n, map<ll, ll>& m) {
+    while (n % 2 == 0) {
+        m[2]++;
+        n /= 2;
+    }
+    for (ll i = 3; i <= sqrt(n); i += 2) {
+        while (n % i == 0) {
+            m[i]++;
+            n /= i;
+        }
+    }
+    if (n > 2)
+        m[n]++;
+}
+
+vector<vector<ld>> dentity(ll n){
+    vector<vector<ld>> v(n, vector<ld>(n, 0));
+    for (int i = 0; i < n; ++i)
+        v[i][i] = 1;
+    return v;
+}
+
+vector<vector<ld>> multiplication(vector<vector<ld>>& a, vector<vector<ld>>& b){
+    ll n = (ll)a.size(), m = (ll)b[0].size();
+    vector<vector<ld>> c(n, vector<ld>(m, 0));
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < m; ++j)
+            for (int k = 0; k < (ll)b.size(); ++k)
+                c[i][j] += a[i][k] * b[k][j];
+    return c;
+}
+
+vector<vector<ld>> exponentiation(vector<vector<ld>>& a, ll p){
+    if (p == 0)
+        return dentity((ll)a.size());
+    vector<vector<ld>> temp = exponentiation(a, p/2);
+    temp = multiplication(temp, temp);
+    if (p&1)
+        temp = multiplication(temp, a);
+    return temp;
+}
+
+int main() {
+    fast();
+    ll t;
+//    cin >> t;
+    t = 1;
+
+    while (t --> 0) {
+        set<ll> s, s1, s2;
+        ll n, k;
+        cin >> n >> k;
+        map<ll, bool> mapp;
+
+        for (int i = 0; i < n; ++i) {
+            ll x;
+            cin >> x;
+            s.insert(x);
+        }
+
+        for (ll m : s){
+            auto it = s.lower_bound(m*k);
+            if (!mapp[m]) {
+                s1.insert(m);
+                if (*it == m * k) {
+                    s1.insert(m);
+                    s2.insert(m * k);
+                    mapp[m * k] = true;
+                }
+            }
+        }
+
+        cout << max((ll)s1.size(), (ll)s2.size()) << endl;
+    }
+    return 0;
+}
