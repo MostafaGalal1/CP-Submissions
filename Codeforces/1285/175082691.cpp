a9158583054d2d991138cfd4afdf543844ba0413
Mostafa Galal <77402549+MostafaGalal1@users.noreply.github.com>
2023-10-10 17:29:25 +0300

    Add problem `Mezo Playing Zoma`

---
+++ Codeforces/1285/175082691.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    cout << n + 1 << endl;
+}
